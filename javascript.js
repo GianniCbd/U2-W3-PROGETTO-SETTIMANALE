6b04fdec69cf01837d94210dc4128df18fcb16e0
GianniCbd <giannicabiddu@gmail.com>
2023-11-10 17:15:24 +0100

    U2-W3-PROGETTO SETTIMANALE

--- javascript.js
+++ javascript.js
@@ -8,15 +8,21 @@ window.onload = () => {
   })
     .then((resp) => {
       if (resp.ok) return resp.json();
+      throw new Error("Errore nella richiesta del server");
     })
     .then((products) => {
       const list = document.querySelector(".list-product");
       products.forEach((elem) => {
         const li = document.createElement("li");
         li.className = "list-group-item d-flex flex-column mt-4 fs-4";
-        li.innerHTML = `<span>${elem.image}</span><span class="me-auto text-danger fs-1">${elem.name}</span> <span class="me-4">${elem.description}</span> <span>${elem.price}€</span> <a href="./dettagli.html?prod=${elem._id}">Scopri di più</a> `;
+
+        const img = document.createElement("img");
+        img.src = elem.imageUrl;
+        li.appendChild(img);
+
+        li.innerHTML += `<span class="me-auto text-danger fs-1">${elem.name}</span> <span class="me-4">${elem.description}</span> <span>${elem.price}€</span> <a href="./dettagli.html?prod=${elem._id}">Scopri di più</a> `;
         list.appendChild(li);
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.error(err));
 };
